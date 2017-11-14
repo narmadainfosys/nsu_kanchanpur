ce4bad953ff931970e957b7f37334ce862867a71
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2017-11-14 15:14:22 +0000

    add social sharing feature

---
+++ config/initializers/social_share_button.rb
@@ -0,0 +1,5 @@
+SocialShareButton.configure do |config|
+  config.allow_sites = %w( facebook twitter google_plus google_bookmark
+                          delicious tumblr pinterest email linkedin wechat 
+                          reddit hacker_news telegram )
+end
