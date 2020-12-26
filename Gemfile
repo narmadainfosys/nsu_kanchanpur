483cb9cde6c140f21db5fe25294993ea56d9a596
Bishisht Bhatta <bishisht@narmadainfosys.com>
2020-12-26 19:09:54 +0545

    Updated versions and did rails app:update

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   gem 'sqlite3'
   gem 'byebug'
+  gem 'listen'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-
-  root 'contact#index'
-  devise_for :users
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
