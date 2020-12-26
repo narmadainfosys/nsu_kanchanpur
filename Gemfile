5f75e6ad005df3ab28cb8ffe5cb2d3fcfc04c898
Bishisht Bhatta <bishisht@narmadainfosys.com>
2020-12-26 19:42:41 +0545

    made some changes

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ group :development, :test do
 end
 
 group :development do
-  gem 'web-console', '~> 2.0'
+  gem 'web-console' 
   gem 'spring'
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'articles/index'
+
+  root 'contact#index'
+  devise_for :users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end
